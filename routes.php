1370e771c3e59765c9264482d925b09b6ae20d30
Juna Panchev <parkbeomjun@naver.com>
2018-01-03 18:35:41 +0900

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-//컨트롤러 경로임
 $route['default_controller'] = 'root';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+## This Added
 $route['sitemap\.xml'] = "sess/sitemap";
